174c0e76715c9bda3171120c4ba34f5f996d6662
Sebas Sebas <seplarui@gmail.com>
2017-01-19 23:10:34 +0100

    Ejemplos PDO

---
+++ Pruebas/Libro/Configuracion.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$servidor="localhost";
+$usuario="alumno";
+$pass="alumno";
+$base_datos="compras";
---
+++ Pruebas/Libro/Ejemplo1PDO.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+    
+    <head>
+        <meta charset="utf8">
+        <title>Ejemplo 1 con PDO</title>
+    </head>
+    
+    <?php
+    
+  
+        
+    require_once 'Configuracion.php';
+    
+    $base_datos_PDO=new PDO("mysql:host=$servidor;dbname=$base_datos;charset=utf8",$usuario,$pass);
+        
+    $resultado= $base_datos_PDO->query("select * from usuarios");
+    
+    /*foreach ($resultado as $row) {
+        
+        echo $row['nombre']. " ".$row['apellidos']." "."<br/>";
+        
+    }*/
+    
+    echo "WHILE<BR/>";
+    
+    while($fila=$resultado->fetch(PDO::FETCH_ASSOC)) {
+        //echo $fila['nombre']. " ".$fila['apellidos']."<br/>";
+        
+        print_r($fila);
+    }
+    
+    print_r($fila);
+    ?>
+    
+</html>
+
