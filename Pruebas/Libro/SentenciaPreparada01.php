952a0ee2ee13bc35fd705907bc7463ccbcc01636
Sebas <seplarui@gmail.com>
2017-01-20 19:14:00 +0100

    SentenciasPreparadas01

---
+++ Pruebas/Libro/SentenciaPreparada01.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'configuracion.php';
+
+$base_datos_PDO=new PDO("mysql:host=$servidor;dbname=$base_datos",$usuario,$pass);
+
+$sql="select id, nombre, apellidos from usuarios where id=:id";
+
+$sentencia=$base_datos_PDO->prepare($sql);
+$sentencia->bindValue(":id",1);
+$sentencia->execute();
+
+$luis=$sentencia->fetch(PDO::FETCH_BOTH);
+$sentencia->bindValue(":id", 2);
+
+$sentencia->execute();
+
+$paco=$sentencia->fetch(PDO::FETCH_BOTH);
+
+print_r($luis);
+print_r($paco);
+
+?>
