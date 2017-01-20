9ca343b32d21aa8f6bbca891d995fcf3cdf0fca1
Sebas <seplarui@gmail.com>
2017-01-20 20:21:55 +0100

    Insertar en BDA

--- Pruebas/Libro/InsertarFilas.php
+++ Pruebas/Libro/InsertarFilas.php
@@ -1,6 +1,19 @@
 <?php
 
+require_once 'configuracion.php';
 
+$base_datos_PDO= new PDO("mysql:host=$servidor;dbname=$base_datos",$usuario,$pass);
 
+$sql="insert into usuarios(id, nombre, apellidos, cuenta)values(:id ,:nombre,:apellidos,:cuenta)";
+
+$sentencia=$base_datos_PDO->prepare($sql);
+$sentencia->bindValue(":id", 17);
+$sentencia->bindValue(":nombre", 'Carlos');
+$sentencia->bindValue(":apellidos", "Álvarez");
+
+$sentencia->bindValue(":cuenta", 3342);
+$sentencia->execute();
+
+echo "El último id es ". $base_datos_PDO->lastInsertId();
 
 ?>
