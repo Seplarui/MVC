cd1ac50f997e198a389a06ba79e7fefa8fb6f7f3
Sebas Sebas <seplarui@gmail.com>
2017-01-22 10:42:56 +0100

    Prueba

--- ModeloV_Vista.php
+++ ModeloV_Vista.php
@@ -1,8 +1,55 @@
-<?php
+<!DOCTYPE HTML>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<html lang="es">
+    
+    
+    <head>
+        
+        <title>Modelo Vista</title>
+        <meta charset="utf-8">
+        
+        <style type="text/css">
+            
+            td {
+                border:1px dotted #FF0000;
+            }
+            
+            
+        </style>
+        
+    </head>
+    
+    <body>
+        
+        <div align="center">
+            
+            <h1 align="center">Gestión de Modelos de Vehículos</h1>
+            <hr>
+            <ul><li><a href="Alta_Modelo.php">Alta Modelo</a></li></ul>
+            
+            <?php
+            
+            //error_reportin(0);
+            
+            include_once 'config.php';
+            include_once 'ModeloV_Controlador';
+            
+            print_r($resultado_modelo);
+            
+            
+            
+            ?>
+            
+            
+        </div>
+        
+        
+    </body>
+        
+    
+    
+    
+    
+    
+    
+</html>
